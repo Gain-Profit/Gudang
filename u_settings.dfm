61ddc73100a8c6486ea243a60131129512c044c9
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-04-04 15:21:40 +0700

    simpan setting

--- u_settings.dfm
+++ u_settings.dfm
@@ -1,7 +1,7 @@
 object FrmSetting: TFrmSetting
   Left = 359
   Top = 125
-  Width = 479
+  Width = 400
   Height = 327
   BorderIcons = [biSystemMenu]
   Caption = 'Setting'
@@ -13,7 +13,40 @@ object FrmSetting: TFrmSetting
   Font.Style = []
   OldCreateOrder = False
   Position = poScreenCenter
-  OnShow = FormShow
+  OnCreate = FormCreate
+  OnDestroy = FormDestroy
   PixelsPerInch = 96
   TextHeight = 13
+  object pnlFooter: TPanel
+    Left = 0
+    Top = 247
+    Width = 384
+    Height = 41
+    Align = alBottom
+    TabOrder = 0
+    DesignSize = (
+      384
+      41)
+    object btnBatal: TButton
+      Left = 304
+      Top = 8
+      Width = 75
+      Height = 25
+      Anchors = [akTop, akRight]
+      Caption = 'Batal'
+      ModalResult = 7
+      TabOrder = 1
+    end
+    object btnSimpan: TButton
+      Left = 224
+      Top = 8
+      Width = 75
+      Height = 25
+      Anchors = [akTop, akRight]
+      Caption = 'Simpan'
+      ModalResult = 6
+      TabOrder = 0
+      OnClick = btnSimpanClick
+    end
+  end
 end
--- u_settings.pas
+++ u_settings.pas
@@ -4,12 +4,18 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, UFungsi;
+  Dialogs, UFungsi, StdCtrls, ExtCtrls;
 
 type
   TFrmSetting = class(TForm)
-    procedure FormShow(Sender: TObject);
+    pnlFooter: TPanel;
+    btnBatal: TButton;
+    btnSimpan: TButton;
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+    procedure btnSimpanClick(Sender: TObject);
   private
+    FCheck : TStringList;
     { Private declarations }
   public
     { Public declarations }
@@ -24,12 +30,72 @@ uses u_dm;
 
 {$R *.dfm}
 
-procedure TFrmSetting.FormShow(Sender: TObject);
-var
-  LSql : string;
+function ubahCB(checkbox: TCheckBox): string;
 begin
-    LSql := 'SELECT * FROM tb_settings';
-    fungsi.SQLExec(dm.Q_temp, LSql, True);
+  if checkbox.Checked = true then
+    result := 'Y'
+  else
+    result := 'N';
+end;
+
+procedure TFrmSetting.FormCreate(Sender: TObject);
+var
+  i, LPosisi : Integer;
+  LSql : string;
+  LCheck : TCheckBox;
+begin
+  FCheck := TStringList.Create;
+
+  LPosisi := 8;
+  LSql := 'SELECT * FROM tb_settings';
+  fungsi.SQLExec(dm.Q_temp, LSql, True);
+  for i := 0 to dm.Q_temp.RecordCount - 1 do
+  begin
+    LCheck := TCheckBox.Create(Self);
+    LCheck.Parent := Self;
+    LCheck.Left := 8;
+    LCheck.Top := LPosisi;
+    LCheck.Width := 370;
+    LCheck.Height := 17;
+    LCheck.TabOrder := i;
+    LCheck.Caption := dm.Q_temp.FieldByName('keterangan').AsString;
+    LCheck.Checked := dm.Q_temp.FieldByName('nilai').AsBoolean;
+    LCheck.Show;
+
+    FCheck.AddObject(dm.Q_temp.FieldByName('id').AsString, LCheck);
+
+    LPosisi := LPosisi + 24;
+    dm.Q_temp.Next;
+  end;
+  Self.Height := LPosisi + 100;
+end;
+
+procedure TFrmSetting.FormDestroy(Sender: TObject);
+begin
+  FCheck.Free;
+end;
+
+procedure TFrmSetting.btnSimpanClick(Sender: TObject);
+var
+  i: Integer;
+  Lid, Lids, LNilai, LNilais, LSql :string;
+begin
+  for i:= 0 to FCheck.Count - 1 do
+  begin
+    Lid := FCheck.Strings[i];
+    LNilai := ubahCB(TCheckBox(FCheck.Objects[i]));
+
+    LNilais := LNilais + Format('WHEN "%s" THEN "%s" ', [Lid, LNilai]);
+    Lids := Lids + Format('"%s", ', [Lid]);
+  end;
+  SetLength(LNilais, Length(LNilais) - 1);
+  SetLength(Lids, Length(Lids) - 2);
+
+  LSql := Format('UPDATE tb_settings SET nilai = (CASE id %s END) WHERE id IN (%s)',
+    [LNilais, Lids]);
+
+  fungsi.SQLExec(dm.Q_Exe, LSql, False);
+  ShowMessage('Penyimpan Data Setting Berhasil...');
 end;
 
 end.
